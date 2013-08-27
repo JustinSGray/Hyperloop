e5092eec84afbdcdc71f65e5b4993f26320d9296
jcchin <jchin89@gmail.com>
2013-08-27 15:48:06 -0400

    Working (not complete)

--- DOE/hyperAssembly.py
+++ DOE/hyperAssembly.py
@@ -2,18 +2,17 @@
     hyperAssembly.py - 
         In support of the open-source Hyperloop transit project
 
-		This openMDAO assembly wraps NPSS and runs a basic DOE
-		
-		-hyperAssembly.py		[top level DOE assembly]
-		-npssCases.py			[NPSS component wrapper]
-		-designInputs.py		[design input variable tree]
-		-./cycle/				[folder containing NPSS model]
-			-hyper.run			[model and run commands]
-			-hyper.view_page	[output viewer]
-			-hyper.viewOut		[actual ouptut]
+        This openMDAO assembly wraps NPSS and runs a basic DOE
+        
+        -hyperAssembly.py       [top level DOE assembly]
+        -npssCases.py           [NPSS component wrapper]
+        -./cycle/               [folder containing NPSS model]
+            -hyper.run          [model and run commands]
+            -hyper.view_page    [output viewer]
+            -hyper.viewOut      [actual ouptut]
 
     Compatible with OpenMDAO v0.8.1
-	Compatible with NPSS v25D2_VC10??
+    Compatible with NPSS v25D2_VC10??
 """
 #Python Imports
 import os.path
@@ -25,59 +24,27 @@ from openmdao.main.api import Assembly, Component, VariableTree, set_as_top, cre
 from openmdao.lib.drivers.api import DOEdriver
 from openmdao.lib.doegenerators.api import FullFactorial
 from openmdao.lib.casehandlers.api import ListCaseRecorder, CSVCaseRecorder, DumpCaseRecorder
-from openmdao.lib.datatypes.api import Float, Array, File, Slot, VarTree
+from openmdao.lib.datatypes.api import Float, Array, File, Slot, VarTree, Str
 from npsscomponent import NPSScomponent
 #External File Imports
-from designInputs import designInputsTree
 from npssCases import DesignCase
-        
-class varAssign(Component):
-    """variable assignment"""
-	
-    designInputs = VarTree(designInputsTree(), iotype='in') 
-    def __init__(self):
-        super(varAssign, self).__init__() 
-        #Do Nothing
-    
-    def execute(self):
-        #override variable values here
-		#redirect files
-		#unit conversions?
-        self.folderTree.DOE_myOutdirectoryL0= "OutputDOE"
-        if not os.path.exists(self.folderTree.DOE_myOutdirectoryL0):
-            os.mkdir(self.folderTree.DOE_myOutdirectoryL0)
-        
-        self.folderTree.DOE_myOutfileName = os.sep+ "VCE_"+str(self.designInputs.T3_opLimit)+"_"+str(self.designInputs.FanPR_des)+"_"+str(self.designInputs.OPR_des)+"_"+str(self.designInputs.T4_des)+"_"+str(self.designInputs.VCEBPR_des)
-        self.folderTree.DOE_myOutdirectoryL1=self.folderTree.DOE_myOutdirectoryL0 + os.sep + "Outfolder_"+str(self.designInputs.T3_opLimit)+"_"+str(self.designInputs.VCEBPR_des)
-        self.folderTree.DOE_myOutdirectoryL2=self.folderTree.DOE_myOutdirectoryL1+self.folderTree.DOE_myOutfileName 
-        self.fileTree.FLOPSformatName =pth+"_FLOPSeng.txt"
-        self.fileTree.indepPageName=pth+"_indeps.output"
-        self.fileTree.noisePageName=pth+"_Noise.output"
-        self.fileTree.ESTformatName=pth+"_NoiseEST.output"
-        self.fileTree.WATEPageName=pth+"_WATE.output" 
-        self.fileTree.WATEDiskName=pth+"_diskpage.output"
-        self.fileTree.WATEVSPName=pth+"_VSP.xml"
-        self.fileTree.WATECostName=pth+"_AutodataCost.output"
-        self.fileTree.myOutfileName=  self.folderTree.DOE_myOutfileName + os.sep
-        self.fileTree.myOutdirectoryL2=  self.folderTree.DOE_myOutdirectoryL2
-		
+
 class hyperTop(Assembly):
     """ Hyperloop Top level Assembly. """
     
     def configure(self):
-        super(VCE_top, self).configure()
+        super(hyperTop, self).configure()
         #--------------------------Component Setup-----------------------------------
-        self.add('varAssign',varAssign()) #variable Assignment imported from designInputs.py
+        
         self.add('NPSS', DesignCase()) #imported from npssCases.py
 
         #---------------------------DOE Setup----------------------------------------
         self.add('driver',DOEdriver())
-        self.driver.add_parameter(('varAssign.designInputs.T3_opLimit','NPSS_MDP.T3_opLimit'), low=1960., high=1960.) #low=1760., high=1960.)
-        self.driver.add_parameter(('varAssign.designInputs.FanPR_des','NPSS_MDP.FanPR_des'), low=3.7, high=3.7) #low=2.8, high=4.6
-        self.driver.add_parameter(('varAssign.designInputs.OPR_des','NPSS_MDP.OPR_des'), low=45., high=45.) #low=36., high=48.
-        self.driver.add_parameter(('varAssign.designInputs.T4_des','NPSS_MDP.T4_opLimit'), low=3560., high=3560.) #low=2910., high=3510
+        self.driver.add_parameter('NPSS.tube_Pt', low=99.0/0.5282, high=300.) #low=1760., high=1960.)
+        self.driver.add_parameter('NPSS.capsule_MN', low=0.6, high=1.) #low=1760., high=1960.)
+        self.driver.add_parameter('NPSS.bearing_Pt', low=11000., high=12000.) #low=1760., high=1960.)
         
-        self.driver.DOEgenerator = FullFactorial(num_levels=1)#7
+        self.driver.DOEgenerator = FullFactorial(num_levels=1)
 
         #------------------------Case Recorders Setup--------------------------------
         #outfile = open('test_jeff.txt','w')
@@ -89,29 +56,18 @@ class hyperTop(Assembly):
         
         #-------------------------- Assembly Setup ----------------------------------
         # Assembly Workflow
-        self.driver.workflow.add(['varAssign','NPSS_MDP','flopsWrap','anoppWrap'])#,'NPSS_CycleDeck'])
+        self.driver.workflow.add(['NPSS'])#
 
-        ## Assembly Connections
-        #varAssign --> NPSS
-        for vars in self.varAssign.designInputsTree.list_vars(): #loop through all file name variables and connect them
-            self.connect('varAssign.designInputsTree.'+vars,'NPSS.'+vars)
+        # Assembly Connections
+
         
-        #--------------------------------------------------------------------------
-        #    Workflow        
-        # ____________            ____________  
-        #|            |          |            |
-        #| Var Assign | -------> |    NPSS    |
-        #|____________|			 |____________|
-        #           
-        #--------------------------------------------------------------------------
 if __name__ == '__main__':
     
     top = hyperTop()
-    
     top.run()
     
-    print 'EIFile read from: ' + top.varAssign.fileTree.FLOPSformatName
-    print 'Range: ' + str(top.flopsWrap.range)
+    print 'Total Power to Air: ' + str(top.NPSS.netPower)
+    print 'Water Flow Rate: ' + str(top.NPSS.waterFlowRate)
 
     #for case in top.driver.recorders[0].get_iterator():
     #    print "T3_opLimit: %f"%(case['designInputs.T3_opLimit'])
--- DOE/npssCases.py
+++ DOE/npssCases.py
@@ -1,20 +1,40 @@
 """
     npssCases.py - 
-        Contains NPSS component wrappers for the Hyperloop Design case
-        
+        Contains NPSS component wrappers for the Hyperloop Design case      
 """
 #OpenMDAO Imports
 from openmdao.main.api import Component, create_io_traits
-from openmdao.lib.datatypes.api import Float, Array, File, Slot
+from openmdao.lib.datatypes.api import Float, Array, File, Slot, VarTree, Str
 from npsscomponent import NPSScomponent
 #External File Imports
-from designInputs import designInputsTree
+#from designInputs import designInputsTree
 
 class DesignCase(NPSScomponent):
     """This class runs the NPSS Hyperloop Design Case"""
     
-    #totWt = Float(iotype='out') #total engine weight (bareWt + wtacc)
-    #bareWt = Float(iotype='out')
+    #Key Cycle Design variables
+    #tube_Pt = Float(99.0/0.5282, units="Pa", iotype='in')  #tube total pressure
+    capsule_MN = Float(1., iotype='in')                    #capsule Mach number
+    bearing_Pt = Float(11000., units="Pa", iotype='in')    #air pad total pressure
+    Qdot_1 = Float(-250969.3,  iotype='in') #heat out from first heat exchange (convert to watts 1000 BTU/hr --> 293.071 Watts)
+    Qdot_2 = Float(-25466.5 ,  iotype='in') #heat out from second heat exchange
+    waterPump_PR = Float(8., iotype='in')                  #water pump (heat exchanger) pressure ratio
+    capsule_BPR = Float(1.45, iotype='in')                 #nozzle to air bearing BPR
+    tube_BPR = Float(2.2285, iotype='in')                  #capsule to tube BPR
+    fanFace_MN = Float(0.6, iotype='in')               #Mach number at the fan face
+
+    #Assumed Variables
+    delHeatVap_Water = Float(2260, units="kJ/kg", iotype='in')#heat of vaporization for water  (at 1atm)
+    bearingWdot = Float(200., units="kg/s", iotype='in')      #bearing air flow rate
+    bearingArea =Float(0,  iotype='in')           # total air pad area
+    inletBC = Float(5000., iotype='in')                       #inlet boundary condition
+    nozzleBC = Float(22572., iotype='in')                     #nozzle boundary condition
+    inletArea = Float(5000.,  iotype='in')        #inlet area
+    nozzleArea = Float(22572.,iotype='in')      #nozzle area
+
+    #Key Cycle Output variables
+    netPower = Float(247., units="kW", iotype='out')        #total energy delta [battery output - (compressor + heat exchanger)]... amount of power released to air as heat
+    waterFlowRate = Float(0.39, units="kg/s", iotype='out') #water flow rate in heat exchanger
     
     def __init__(self):
         self.force_execute = True
@@ -35,30 +55,29 @@ class DesignCase(NPSScomponent):
         super(DesignCase, self).configure() #this needs to be first
         self.run_command = 'run();'
         #self.reload_flag = 'hyperReload'
-		
+
         ##generate list of tuples as inputs for every variable in designInputs                     
         #create_io_traits(self, [("designIpnuts." + str(vars), str(vars)) for vars in self.designInputs.list_vars()], iotype='in') #promote NPSS variables and give alias (2nd argument in tuple)
-								#Pass from openMDAO to NPSS
-        create_io_traits(self, [("designInputs.tube_Pt","tube_Pt"),
-                                ("designInputs.capsule_MN","capsule_MN"),
-                                ("designInputs.bearing_Pt","bearing_Pt"),
-                                ("designInputs.waterPump_PR","waterPump_PR")
-								], iotype='in')
+                            #Pass from openMDAO to NPSS
+
+        #varAssign --> NPSS
+        #for vars in self.varAssign.designInputsTree.list_vars(): #loop through all file name variables and connect them
+        #    self.connect('varAssign.designInputsTree.'+vars,'NPSS.'+vars)
+
+        create_io_traits(self, ["tube.Pt",
+                                ], iotype='in')
         create_io_traits(self,[ #(Actual Variable Name, OpenMDAO alias) 
                                 #Promote as openMDAO output variables
                                 ("nozzle.Fl_O.Pt","nozzle_Pt"),
-								("nozzle.Fl_O.W","nozzle_W")
+                                ("nozzle.Fl_O.W","nozzle_W")
                                 ], iotype='out')
         
     def execute(self): #trace the NPSS component with print statements before and after execution
         print self.get_pathname(), '--NPSS design execution begins--'
        
         super(DesignCase, self).execute()
-        
-        #self.bareWt = (self.inletWt - self.engMountWt)
-        #self.weng = (self.bareWt+self.wtacc)
-        
-        print "Total Power: %s (kW)" %self.V1
-        print "Water Flow Rate: %s (kg/s)" %self.waterFlowRate
+                
+        print "Nozzle Pt: %s (Pa)" %self.nozzle_Pt
+        print "Nozzle W: %s (kg/s)" %self.nozzle_W
 
         print self.get_pathname(), '--NPSS design execution complete!--'

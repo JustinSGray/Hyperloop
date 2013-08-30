2d5607aa77a370051c244a777a3ddff7a056b665
Justin Gray <justin.s.gray@gmail.com>
2013-08-30 14:34:45 -0400

    bearing sizing works now

--- geometry/bearing_sizing.py
+++ geometry/bearing_sizing.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, sin
 
 from openmdao.main.api import Component
 from openmdao.lib.datatypes.api import Float, Int
@@ -9,25 +9,30 @@ class BearingSizing(Component):
     tube_radius = Float(4, iotype="in", units="m", desc="radius of the tube")
     capsule_mass = Float(15000, iotype="in", units="kg", desc="mass of capsule")
     pressure = Float(9.4, iotype="in", units="kPa", desc="air injection pressure for bearings")
-    n_bearings = Int(6, iotype="in", desc="number of independent bearing pads") 
-    sweep_angle = Float(20, iotype="in", units="deg", desc="sweep angle of a single bearing pad on tube wall")
+    n_bearings = Int(7, iotype="in", desc="number of rows of bearing pads") 
+    sweep_angle = Float(8, iotype="in", units="deg", desc="sweep angle of a single bearing pad on tube wall")
 
     total_area = Float(iotype="out", units="m**2", desc="total required bearing area")
     area_per_bearing = Float(iotype="out", units="m**2", desc="required area per bearing")
     length_per_bearing = Float(iotype="out", units="m", desc="required length per bearing")
+    bearing_width = Float(iotype="out", units="m", desc="lienar width of bearing")
 
     def execute(self): 
 
         req_area = self.capsule_mass*9.81/(self.pressure*1000) #convert to Pa from kPa
         req_area *= 1.5 #dirty hack to account for pressure gradient under bearing
-        
-        arc_length = self.tube_radius*self.sweep_angle*pi/180
+        sweep_radians = self.sweep_angle*pi/180
+        arc_length = self.tube_radius*sweep_radians
 
         req_total_length = req_area/arc_length
 
         self.total_area = req_area
+        self.bearing_width = 2*self.tube_radius*sin(sweep_radians/2)
+
+        #divide by two because there are two parallel skis
         self.length_per_bearing = req_total_length/self.n_bearings/2
         self.area_per_bearing = req_area/self.n_bearings/2
+
 
 if __name__ == "__main__": 
 
@@ -40,5 +45,6 @@ if __name__ == "__main__":
     print "total_area (m**2): %f"%comp.total_area
     print "area_per_bearing (m**2): %f"%comp.area_per_bearing
     print "length_per_bearing (m): %f"%comp.length_per_bearing
+    print "bearing_width (m): %f"%comp.bearing_width
 
 

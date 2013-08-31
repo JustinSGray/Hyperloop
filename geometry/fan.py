8caa65348f0ccfe88b18740460531d9a81fa9a4d
Justin Gray <justin.s.gray@gmail.com>
2013-08-31 09:21:15 -0400

    re-organizing some folders

--- geometry/fan.py
+++ geometry/fan.py
@@ -4,7 +4,7 @@ from openmdao.main.api import Component
 from openmdao.lib.datatypes.api import Float
 
 
-class FanSizing(Component): 
+class Fan(Component): 
 
     hub_to_tip = Float(.4, iotype="in", desc="hub to tip ratio for the fan")
     flow_area = Float(.4, iotype="in", units="cm**2", desc="required flow area for the fan")
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     from openmdao.main.api import set_as_top
 
-    f = set_as_top(FanSizing())
+    f = set_as_top(Fan())
     f.hub_to_tip = .4
     f.flow_area = 14400
     f.execute()
---
+++ hyperloop_sim.py
@@ -0,0 +1,10 @@
+from openmdao.main.api import Assembly
+
+from geometry.battery import Battery
+from geometry.bypass_duct import BypassDuct
+from geometry.fan import Fan
+
+class Hyperloop(Assembly): 
+
+    def configure(self): 
+

358b1648b66c031a69f091912f841ca9ae16e295
jcchin <jchin89@gmail.com>
2013-09-09 15:54:24 -0400

    Added more input/out variables, added comments for calculation workflow

--- cycle/heatExchanger.py
+++ cycle/heatExchanger.py
@@ -1,6 +1,7 @@
 """
     heatExchanger.py - 
-        Performs basic heat exchanger calculations
+        Performs basic heat exchanger calculations for a single pass
+        counter-flow heat exchanger
         
 Logarithmic Mean Temperature Difference (LMTD) Method
     Design a heat exchanger to meet prescribed heat transfer requirements
@@ -23,9 +24,23 @@ NTU (effectiveness) Method
 from openmdao.main.api import Component
 from openmdao.lib.datatypes.api import Float
 
+from math import log
+
 
 class heatExchanger(Component):
     """ Main Component """
+
+    #--Inputs--
+    #Boundary Temperatures
+    T_win = Float(0.0, iotype='in', desc='Temp of water into heat exchanger')
+    T_wout = Float(0.0, iotype='in', desc='Temp of water out of heat exchanger')
+    T_ain = Float(0.0, iotype='in', desc='Temp of air into heat exchanger')
+    T_aout = Float(0.0, iotype='in', desc='Temp of air out of heat exchanger')
+
+    #Design Variables
+    Mdot_w = = Float(0.0, iotype='in', desc='Mass flow rate of water pumped through system')
+    Ao_pipe = Float(0.0, iotype='out', desc='Shell pipe (Outer) Area')
+    Ai_pipe = Float(0.0, iotype='out', desc='Tube pipe (Inner) Area')
 
     #Assumed Constant Properties
     rho_w = Float(0.0, iotype='in', desc='density of water')
@@ -40,8 +55,12 @@ class heatExchanger(Component):
     k_a = Float(0.0, iotype='in', desc='thermal conductivity for air')
     k_p = Float(0.0, iotype='in', desc='thermal conductivity of the pipe')
 
-    #Calculated Variables
+    #--Outputs--
+    #Intermediate Variables
+    Asurf_pipe = Float(0.0, iotype='out', desc='Surface Area of the Pipe')
 
+
+    #Calculated Variables
     V_w = Float(0.0, iotype='out', desc='flow velocity of water')
     V_a = Float(0.0, iotype='out', desc='flow velocity of air')
     W_w = Float(0.0, iotype='out', desc='mass flow of water')
@@ -50,21 +69,41 @@ class heatExchanger(Component):
     h_a = Float(0.0, iotype='out', desc='heat transfer of air')
     q_w = Float(0.0, iotype='out', desc='heat flow of water')
     q_a = Float(0.0, iotype='out', desc='heat flow of air')
-    T_win = Float(0.0, iotype='out', desc='Temp of water into heat exchanger')
-    T_wout = Float(0.0, iotype='out', desc='Temp of water out of heat exchanger')
-    T_ain = Float(0.0, iotype='out', desc='Temp of air into heat exchanger')
-    T_aout = Float(0.0, iotype='out', desc='Temp of air out of heat exchanger')
+    
+    #Size/Volume Considerations
+    V_water = Float(0.0, iotype='out', desc='Volume of input water tank')
+    V_steam = Float(0.0, iotype='out', desc='Volume of output steam tank')
+    M_water = Float(0.0, iotype='out', desc='Mass of input water tank')
+    M_steam = Float(0.0, iotype='out', desc='Mass of output steam tank')
 
-    Asurf_pipe = Float(0.0, iotype='out', desc='Surface Area of the Pipe')
-    Ao_pipe = Float(0.0, iotype='out', desc='Outer Area of the pipe')
-    Ai_pipe = Float(0.0, iotype='out', desc='Inner Area of the pipe')
 
 
     def execute(self):
-        """Calculate LMTD
+        """Calculate Various Paramters
         """
 
-        LMTD = self.LMTD
+        #Determine amount of heat that needs to be transferred
+        #Q_water must equal Q_air
+        #Q = Mdot * Cp * delta T
+
+
+
+        #Determine the flow rate of the water
+        # Mdot = rho * Area * Volume
+
+
+        #Determine Hyrdraulic Diameters
+
+
+
+        #Determine Overall Heat Transfer Coefficient
+        #1) Calculate Reynolds Number
+        #2) Calculate Prandtl Number
+        #3) Calculate Nusselt Number
+
+
+        #Determine LMTD
+        LMTD = self.LMTD #Log Mean Temp Diff
         Th_in = self.T_ain
         Tc_in = self.T_win
         Th_out = self.T_aout
@@ -73,4 +112,16 @@ class heatExchanger(Component):
         dT1 = (Th_out - Th_in) #Change in T1 (delta T1)
         dT2 = (Tc_out-Tc_in) #Change in T2 (delta T2)
 
-        self.LMTD = (dT1 - dT2)/(ln(dT1/(dT2)))
+        self.LMTD = (dT1 - dT2)/(log(dT1/(dT2), e)) #take natural log (base e)
+
+
+
+        #Determine the required length of the heat exchanger
+
+
+
+        #Assume fouling losses
+
+
+
+        #Assume pipe minor losses

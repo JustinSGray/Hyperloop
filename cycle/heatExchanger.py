32a49bbdafb1a64d094366f3414101d2afa07e3c
jcchin <jchin89@gmail.com>
2013-09-09 18:02:11 -0400

    Added units, initial engineering calcs

--- cycle/heatExchanger.py
+++ cycle/heatExchanger.py
@@ -1,7 +1,7 @@
 """
     heatExchanger.py - 
         Performs basic heat exchanger calculations for a single pass
-        counter-flow heat exchanger
+        counter-flow shell and tube heat exchanger
         
 Logarithmic Mean Temperature Difference (LMTD) Method
     Design a heat exchanger to meet prescribed heat transfer requirements
@@ -24,7 +24,7 @@ NTU (effectiveness) Method
 from openmdao.main.api import Component
 from openmdao.lib.datatypes.api import Float
 
-from math import log
+from math import log, pi
 
 
 class heatExchanger(Component):
@@ -32,68 +32,101 @@ class heatExchanger(Component):
 
     #--Inputs--
     #Boundary Temperatures
-    T_win = Float(0.0, iotype='in', desc='Temp of water into heat exchanger')
-    T_wout = Float(0.0, iotype='in', desc='Temp of water out of heat exchanger')
-    T_ain = Float(0.0, iotype='in', desc='Temp of air into heat exchanger')
-    T_aout = Float(0.0, iotype='in', desc='Temp of air out of heat exchanger')
+    T_win = Float(0.0, units = 'K', iotype='in', desc='Temp of water into heat exchanger')
+    T_wout = Float(0.0, units = 'K', iotype='in', desc='Temp of water out of heat exchanger')
+    T_ain = Float(0.0, units = 'K', iotype='in', desc='Temp of air into heat exchanger')
+    T_aout = Float(0.0, units = 'K', iotype='in', desc='Temp of air out of heat exchanger')
 
     #Design Variables
-    Mdot_w = = Float(0.0, iotype='in', desc='Mass flow rate of water pumped through system')
-    Ao_pipe = Float(0.0, iotype='out', desc='Shell pipe (Outer) Area')
-    Ai_pipe = Float(0.0, iotype='out', desc='Tube pipe (Inner) Area')
+    Mdot_w = Float(0.0, units = 'kg/s', iotype='in', desc='Mass flow rate of water pumped through system')
+    Mdot_a = Float(0.0, units = 'kg/s', iotype='out', desc='Mass flow rate of air')
+    Di_shell = Float(0.0, units = 'm', iotype='out', desc='Shell pipe (inner) Diameter')
+    Do_tube = Float(0.0, units = 'm', iotype='out', desc='Tube pipe (outer) Diameter')
+    Di_tube = Float(0.0, units = 'm', iotype='out', desc='Tube pipe (inner) Diameter')
 
     #Assumed Constant Properties
-    rho_w = Float(0.0, iotype='in', desc='density of water')
-    rho_a = Float(0.0, iotype='in', desc='density of air ')
-    cp_w = Float(0.0, iotype='in', desc='specific heat of water')
-    cp_a = Float(0.0, iotype='in', desc='specific heat of air')
-    dvisc_w = Float(0.0, iotype='in', desc='dynamic viscosity for water')
-    dvis_a = Float(0.0, iotype='in', desc='dynamic viscosity for air')
-    kvisc_w = Float(0.0, iotype='in', desc='kinematic viscosity for water')
-    kvisc_a = Float(0.0, iotype='in', desc='kinematic viscosity for air')
-    k_w = Float(0.0, iotype='in', desc='thermal conductivity for water')
-    k_a = Float(0.0, iotype='in', desc='thermal conductivity for air')
-    k_p = Float(0.0, iotype='in', desc='thermal conductivity of the pipe')
+    rho_w = Float(0.0, units = 'kg/m**3', iotype='in', desc='density of water')
+    rho_a = Float(0.0, units = 'kg/m**3', iotype='in', desc='density of air ')
+    cp_w = Float(0.0, units = 'J/(kg*K)', iotype='in', desc='specific heat of water')
+    cp_a = Float(0.0, units = 'J/(kg*K)', iotype='in', desc='specific heat of air')
+    dvisc_w = Float(0.0, units = 'kg/(m*s)', iotype='in', desc='dynamic viscosity for water')
+    dvis_a = Float(0.0, units = 'kg/(m*s)', iotype='in', desc='dynamic viscosity for air')
+    kvisc_w = Float(0.0, units = 'm**2/s', iotype='in', desc='kinematic viscosity for water')
+    kvisc_a = Float(0.0, units = 'm**2/s', iotype='in', desc='kinematic viscosity for air')
+    k_w = Float(0.0, units = 'W/(m*K)', iotype='in', desc='thermal conductivity for water')
+    k_a = Float(0.0, units = 'W/(m*K)', iotype='in', desc='thermal conductivity for air')
+    k_p = Float(0.0, units = 'W/(m*K)', iotype='in', desc='thermal conductivity of the pipe')
 
     #--Outputs--
     #Intermediate Variables
-    Asurf_pipe = Float(0.0, iotype='out', desc='Surface Area of the Pipe')
-
+    Asurf_pipe = Float(0.0, units = 'm**2', iotype='out', desc='Surface Area of the Pipe')
+    Dh = Float(0.0, units= 'm', iotype='out', desc='Hyrdraulic Diameter of the shell (annulus) for fluid flow')
+    De = Float(0.0, units= 'm', iotype='out', desc='Hyrdraulic Diameter of the shell (annulus) for heat flow')
 
     #Calculated Variables
-    V_w = Float(0.0, iotype='out', desc='flow velocity of water')
-    V_a = Float(0.0, iotype='out', desc='flow velocity of air')
-    W_w = Float(0.0, iotype='out', desc='mass flow of water')
-    W_a = Float(0.0, iotype='out', desc='mass flow of air')
-    h_w = Float(0.0, iotype='out', desc='heat transfer of water')
-    h_a = Float(0.0, iotype='out', desc='heat transfer of air')
-    q_w = Float(0.0, iotype='out', desc='heat flow of water')
-    q_a = Float(0.0, iotype='out', desc='heat flow of air')
+    V_w = Float(0.0, units= 'm/s', iotype='out', desc='flow velocity of water')
+    V_a = Float(0.0, units= 'm/s', iotype='out', desc='flow velocity of air')
+    h_w = Float(0.0, iotype ='out', desc='heat transfer of water')
+    h_a = Float(0.0, units = 'W/(m**2)*K', iotype='out', desc='heat transfer of air')
+    q_w = Float(0.0, units = 'W', iotype='out', desc='heat flow of water')
+    q_a = Float(0.0, units = 'W', iotype='out', desc='heat flow of air')
     
     #Size/Volume Considerations
-    V_water = Float(0.0, iotype='out', desc='Volume of input water tank')
-    V_steam = Float(0.0, iotype='out', desc='Volume of output steam tank')
-    M_water = Float(0.0, iotype='out', desc='Mass of input water tank')
-    M_steam = Float(0.0, iotype='out', desc='Mass of output steam tank')
+    Vol_water = Float(0.0, units= 'm**3', iotype='out', desc='Volume of input water tank')
+    Vol_steam = Float(0.0, units= 'm**3', iotype='out', desc='Volume of output steam tank')
+    Mass_water = Float(0.0, units= 'kg', iotype='out', desc='Mass of input water tank')
+    Mass_steam = Float(0.0, units= 'kg', iotype='out', desc='Mass of output steam tank')
 
 
 
     def execute(self):
-        """Calculate Various Paramters
-        """
+        """Calculate Various Paramters"""
 
-        #Determine amount of heat that needs to be transferred
-        #Q_water must equal Q_air
-        #Q = Mdot * Cp * delta T
-
-
-
-        #Determine the flow rate of the water
-        # Mdot = rho * Area * Volume
+        #Determine the area of the air tube
+        A_a = pi*(self.Di_tube/2)^2
+        #Determine the fluid velocity of the air
+        #Rearrange Mdot = rho * Area * Velocity --> Velocity = Mdot/(rho*Area)
+        Veloc_a = Mdot_a / (rho_a * A_a)
 
 
-        #Determine Hyrdraulic Diameters
+        #Determine q
+        #q = mdot * cp * deltaT
+        q_a = Mdot_a* cp_a * (T_aout - T_ain)
 
+        #Energy Balance: Q_water must equal Q_air
+        q_w = q_a
+
+        #Determine water Mdot
+        #q = mdot * cp * deltaT
+        Mdot_w = q_w / cp_w * (T_win - T_wout)
+        #Determine the flow velocity of the water
+        #Rearrange Mdot = rho * Area * Velocity --> Velocity = Mdot/(rho*Area)
+        Veloc_w = Mdot_w / (rho_w * A_w)
+
+
+        #Hydraulic Diameter
+        #D_h = (4*Af)/(Pflow) = 4*pi*(Di_shell^2 - Do_tube^2)/ 4*pi*(Di_shell - Do_tube) = Di_shell - Do_tube
+        #D_e = (4*Af)/(PheatTransfer) = 4*pi*(Di_shell^2 - Do_tube^2)/ 4*pi* Do_tube = (Di_shell^2 - Do_tube^2)/Do_tube
+
+        Dw_h = self.Di_shell - Do_tube
+        Dw_e = (self.Di_shell^2 - self.Do_tube^2)/self.Do_tube
+
+        Da_h = self.Di_shell - Do_tube
+        Da_e = (self.Di_shell^2 - self.Do_tube^2)/self.Do_tube
+
+        #Determine the Reynolds Number
+        #Re = velocity * hydraulic dimater / kinematic viscostiy   (general form for pipes)
+        #Re = inertial forces/ viscous forces
+        Re_a = Veloc_a*Da_h/self.kvisc_a
+        Re_w = Veloc_w*Dw_h/self.kvisc_w
+
+        
+        #Determine the Prandtl Number
+        #Pr = viscous diffusion rate/ thermal diffusion rate = Cp * dyanamic viscosity / thermal conductivity
+        #Pr << 1 means thermal diffusivity dominates
+        #Pr >> 1 means momentum diffusivity dominates
+        Pr_a = 
+        Pr_w = 
 
 
         #Determine Overall Heat Transfer Coefficient

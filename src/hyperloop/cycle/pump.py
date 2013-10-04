c948af49ba85e063dcdda9baa4d9e4e4f87970bc
Justin Gray <justin.s.gray@gmail.com>
2013-10-04 18:54:27 -0400

    changed flowstation class name

--- src/hyperloop/cycle/pump.py
+++ src/hyperloop/cycle/pump.py
@@ -6,4 +6,6 @@ from openmdao.lib.datatypes.api import Float
 class Pump(Component): 
     """Calculate the power requirement for a liquid pump given flow conditions""" 
 
-    delta_P
+    Pt_out = Float(100, iotype="in", units="kPa", desc="Pump output pressure")
+    Pt_in = Float(100, iotype="in", units="kPa", desc="Pump output pressure")
+    Pt_out = Float(100, iotype="in", units="kPa", desc="Pump output pressure")

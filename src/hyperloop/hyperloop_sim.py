b1289100c2b9bf3bb48850b1bfc240e598a41642
jcchin <jchin89@gmail.com>
2013-10-07 23:10:02 -0400

    moved energy calc to mission, small clean-up throughout

--- src/hyperloop/hyperloop_sim.py
+++ src/hyperloop/hyperloop_sim.py
@@ -11,7 +11,7 @@ class HyperloopPod(Assembly):
     Mach_pod_max = Float(1.0, iotype="in", desc="travel Mach of the pod")
     #radius_tube = Float(111.5 , iotype="in", units="cm", desc="required radius for the tube")
     Ps_tube = Float(99, iotype="in", desc="static pressure in the tube", units="Pa", low=0)     
-    SolarHeatingFactor = Float(.7, iotype="in", 
+    solar_heating_factor = Float(.7, iotype="in", 
       desc="Fractional amount of solar radiation to consider in tube temperature calculations", 
       low=0, high=1)
 
@@ -40,7 +40,7 @@ class HyperloopPod(Assembly):
         self.connect('pod.radius_inlet_outer', 'flow_limit.radius_inlet')
         self.create_passthrough('flow_limit.Mach_bypass')
         #Hyperloop -> TubeWallTemp
-        self.connect('SolarHeatingFactor', 'tube_wall_temp.nn_incidence_factor')
+        self.connect('solar_heating_factor', 'tube_wall_temp.nn_incidence_factor')
         self.connect('tube_length', 'tube_wall_temp.length_tube')
 
         #Inter-component Connections
@@ -49,13 +49,13 @@ class HyperloopPod(Assembly):
         #Compress -> Pod
         self.connect('compress.area_c1_in', 'pod.area_inlet_out')
         self.connect('compress.area_inlet_in', 'pod.area_inlet_in')
-        self.connect('compress.pwr_req','pod.pwr_req')
         self.connect('compress.rho_air', 'pod.rho_air')
         #Compress -> TubeWallTemp
         self.connect('compress.nozzle_Fl_O', 'tube_wall_temp.nozzle_air')
         self.connect('compress.bearing_Fl_O', 'tube_wall_temp.bearing_air')
         #Mission -> Pod
         self.connect('mission.time','pod.time_mission')
+        self.connect('mission.energy', 'pod.energy')
 
         #Add Solver
         driver = self.add('driver',BroydenSolver())
@@ -63,7 +63,7 @@ class HyperloopPod(Assembly):
         driver.tol = .0001
         #Add Parameters and Constraints
         driver.add_parameter('compress.W_in',low=-1e15,high=1e15)
-        driver.add_parameter(['compress.Ts_tube','flow_limit.Ts_tube','tube_wall_temp.temp_tube_wall'], low=-1e-15, high=1e15)
+        driver.add_parameter(['compress.Ts_tube','flow_limit.Ts_tube','tube_wall_temp.temp_boundary'], low=-1e-15, high=1e15)
         driver.add_parameter(['compress.radius_tube','flow_limit.radius_tube', 'pod.radius_tube_inner'], low=-1e15, high=1e15)
         driver.add_constraint('10*(compress.W_in-flow_limit.W_excess) = 0')
         driver.add_constraint('tube_wall_temp.ss_temp_residual=0')
@@ -102,5 +102,5 @@ if __name__=="__main__":
     print "energy: ", hl.pod.energy
     print "travel time: ", hl.mission.time/60
     print "speed:", hl.compress.speed_max
-    print "Tube Temp: ", hl.tube_wall_temp.temp_tube_wall
+    print "Tube Temp: ", hl.tube_wall_temp.temp_boundary
 

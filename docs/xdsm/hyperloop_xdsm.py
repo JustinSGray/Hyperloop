bd7ffa1b588ce255c99003a8744232b4f662d3c9
Justin Gray <justin.s.gray@gmail.com>
2013-10-08 22:31:56 -0400

    made another xdsm

--- docs/xdsm/hyperloop_xdsm.py
+++ docs/xdsm/hyperloop_xdsm.py
@@ -7,6 +7,7 @@ anl = 'Analysis'
 
 x = XDSM()
 #x.addComp('driver', mda, 'Solver')
+x.addComp('assembly inputs', anl, 'assembly inputs')
 x.addComp('compress', anl, 'compress')
 x.addComp('mission', anl, 'mission')
 x.addComp('pod', anl, 'pod')
@@ -23,6 +24,14 @@ x.addDep('tube_wall_temp', 'compress', dat, '', stack=True)
 x.addDep('compress', 'flow_limit', dat, '', stack=True)
 x.addDep('compress', 'tube_wall_temp', dat, '', stack=True)
 x.addDep('compress', 'pod', dat, '', stack=True)
+
+#assembly inputs
+x.addDep('compress', 'assembly inputs', dat, '', stack=True)
+x.addDep('mission', 'assembly inputs', dat, '', stack=True)
+x.addDep('flow_limit', 'assembly inputs', dat, '', stack=True)
+x.addDep('tube_wall_temp', 'assembly inputs', dat, '', stack=True)
+x.addDep('pod', 'assembly inputs', dat, '', stack=True)
+
 
 #x.addDep('compress','driver', dat, '$W_{in}$')
 

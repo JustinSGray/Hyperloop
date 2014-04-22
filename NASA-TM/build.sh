6a528ff1625ec16970ea637f7962649b627debd2
jcchin <jchin89@gmail.com>
2014-04-22 16:16:13 -0400

    manually fixed references.. thanks LaTeX!

--- NASA-TM/build.sh
+++ NASA-TM/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #build pdf (with bibtex)
 "/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
-bibtex heading.aux
-"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
-makeindex heading.nlo  -s nomencl.ist -o heading.nls
-"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
+#bibtex heading.aux
+#"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
+#makeindex heading.nlo  -s nomencl.ist -o heading.nls
+#"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
 
 mv heading.pdf hyperloop.pdf
 

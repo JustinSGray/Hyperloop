79830baf06d6b17f6762a47081b1bfd6159b8a15
jcchin <jchin89@gmail.com>
2014-06-20 16:29:40 -0400

    saved areaPlot

--- src/hyperloop/cycle/size_plot.py
+++ src/hyperloop/cycle/size_plot.py
@@ -40,9 +40,11 @@ pylab.annotate("     Available pod area \ndecreasing relative to tube", fontsize
         rotation=90, textcoords='offset points', bbox=dict(boxstyle="square", facecolor='lightgrey') )
  
 #              x,    y,   dx,   dy,
-pylab.arrow( 1.121, 0.441, 0.0, 0.10, fc='lightgrey', ec='k', head_width=0.16, head_length=0.070 )
+pylab.arrow( 1.121, 0.458, 0.0, 0.10, fc='lightgrey', ec='k', head_width=0.16, head_length=0.070 )
 
 pylab.vlines(1.0, 0, 1.0, colors='darkgrey', linestyles='dashed',lw= 3)#, label="limit")
+
+pylab.gcf().set_size_inches(11,8)
 
 pylab.show()
 

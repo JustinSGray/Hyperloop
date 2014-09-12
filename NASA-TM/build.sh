bee950c47db72cae994abd1993d7d24b644af96a
jcchin <jchin89@gmail.com>
2014-09-12 10:02:34 -0400

    better build script

--- NASA-TM/build.sh
+++ NASA-TM/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #build pdf (with bibtex)
 "/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
-bibtex heading.aux
-makeindex heading.nlo  -s nomencl.ist -o heading.nls
-"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
-"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
 
-mv heading.pdf hyperloop.pdf
+if [ "$1" == "-f" ]; then #add -f flag to do full build
+	bibtex heading.aux
+	makeindex heading.nlo  -s nomencl.ist -o heading.nls
+	"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
+	"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
+
+	mv heading.pdf hyperloop.pdf
+fi
+
 
 #. clean.sh
